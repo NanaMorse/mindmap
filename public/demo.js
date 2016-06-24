22b239694bb29dfebab85cc42da090d9e52b126f
Morse_Guo <nana.morse@outlook.com>
2016-06-24 23:39:49 +0800

    Change state structure for nested topics

--- public/demo.js
+++ public/demo.js
@@ -3,10 +3,26 @@ export default {
     "bgColor" : "#fff"
   },
 
-  "topic" : {
-    "text" : "Central Topic",
-    "fontSize" : "16px",
-    "shapeClass" : "rect",
-    "fillColor" : "rgb(203, 222, 253)"
+  "topics" : {
+    "style" : {
+      "fontSize" : "16px",
+      "shapeClass" : "rect",
+      "fillColor" : "rgb(203, 222, 253)"
+    },
+    "feed" : ["0", "1", "2"],
+    "topicById" : {
+      "0" : {
+        "parentId" : null,
+        "text" : "Central Topic"
+      },
+      "1" : {
+        "parentId" : "0",
+        "text" : "Child Topic 1"
+      },
+      "2" : {
+        "parentId" : "0",
+        "text" : "Child Topic 2"
+      }
+    }
   }
 };
