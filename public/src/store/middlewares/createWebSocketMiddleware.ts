1ff025eb9ed4cf47dee9f3c88b210804d03bbf85
Morse_Guo <nana.morse@outlook.com>
2016-11-17 22:38:34 +0800

    Filter sync store action

--- public/src/store/middlewares/createWebSocketMiddleware.ts
+++ public/src/store/middlewares/createWebSocketMiddleware.ts
@@ -2,11 +2,11 @@ interface options {
   msgType?: string;
 }
 
-interface actionSocketOptions extends options{
+interface socketOptions extends options{
   broadcastTriggerFilter?: string;
 }
 
-export const createActionSocketMiddleware = (ws, {broadcastTriggerFilter = '_isBroadcast', msgType = 'syncAction'}: actionSocketOptions = {}) => ({getState, dispatch}) => next => action => {
+export const createActionSocketMiddleware = (ws, {broadcastTriggerFilter = '_isBroadcast', msgType = 'syncAction'}: socketOptions = {}) => ({getState, dispatch}) => next => action => {
   next(action);
 
   if (!action[broadcastTriggerFilter]) {
@@ -19,13 +19,15 @@ export const createActionSocketMiddleware = (ws, {broadcastTriggerFilter = '_isB
   }
 };
 
-export const createSyncStoreSocketMiddleware = (ws, {msgType = 'syncStore'}: options = {}) => ({getState, dispatch}) => next => action => {
+export const createSyncStoreSocketMiddleware = (ws, {broadcastTriggerFilter = '_isBroadcast', msgType = 'syncStore'}: socketOptions = {}) => ({getState, dispatch}) => next => action => {
   next(action);
 
-  const sendMsg = {
-    type: msgType,
-    data: JSON.stringify(getState())
-  };
+  if (!action[broadcastTriggerFilter]) {
+    const sendMsg = {
+      type: msgType,
+      data: JSON.stringify(getState())
+    };
 
-  ws.send(JSON.stringify(sendMsg));
+    ws.send(JSON.stringify(sendMsg));
+  }
 };
